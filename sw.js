8344383324c3d1f4a88c3f1fbef24cd92c174d63
amirferjani <amir.fferjani@gmail.com>
2026-07-24 09:06:20 +0200

    Cache integrity-verified synchronized app bundle

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='registratiekassa-zoo-v8';
-const APP_PARTS=Array.from({length:7},(_,index)=>`./assets/app-sync.${String(index+1).padStart(2,'0')}.b64`);
+const CACHE='registratiekassa-zoo-v9';
+const APP_PARTS=Array.from({length:7},(_,index)=>`./assets/app.full.${String(index+1).padStart(2,'0')}.b64`);
 const FILES=['./','./index.html','./loader.js','./manifest.webmanifest','./icons/icon.svg','./assets/styles.css.gz.b64',...APP_PARTS];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
